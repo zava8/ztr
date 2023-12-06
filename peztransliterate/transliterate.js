09aa5983bd7306384f7d97218357b90c2475acd1
zava810 <vimalkg@gmail.com>
2023-12-06 16:12:20 +0530

    added ztrclick phur button

--- peztransliterate/transliterate.js
+++ peztransliterate/transliterate.js
@@ -1,45 +1,27 @@
 // const u_to_a = require('./u_to_a.js');
 var inputText = document.querySelector('#input-text');
 var outputText = document.querySelector('#output-text');
-var buttonTranslate = document.querySelector('#btn-translate');
-var apiUrl = "https://api.funtranslations.com/translate/minion.json";
-
+// var buttonTranslate = document.querySelector('#btn-translate');
+// var apiUrl = "https://api.funtranslations.com/translate/minion.json";
+var selectedztr = null;
 function errorHandle(error) { 
 	// In case error occurs the errorHandle
 	// function will be called
 	alert('Error occurred')
 	console.log("error occurred", error);
 }
-
-function clickHandler() {	 
-	// When someone clicks on translate
-	// button clickHandler will be called
-	var text = inputText.value;
-	var updatedUrl = apiUrl + "?text=" + text;
-	fetch(updatedUrl).then(response =>
-	response.json()).then(json =>
-	outputText.innerText = 
-		(json.contents.translated)).catch(errorHandle);
-}
-
-
-
-buttonTranslate.addEventListener("click", clickHandler); 
-// Adding the event listener click
-////////////
 function getComboA(selectObject) {
-  var text = inputText.value;
-  var value = selectObject.value;  
-	switch(value) {
-		case "unicode_to_inglish" :
-		  outputText.innerText = u_to_a(text,false);
-		break;
-		case "inglish_to_vovelhindi" :
-		  outputText.innerText = i_to_vhin(text);
-		break;
-		case "unicode_to_vovelhindi" :
-		  // alert(text);
-		  outputText.innerText = u_to_u8(text,false);
-		break;i_to_vhin
+  selectedztr = selectObject.value;
+  doztr(selectedztr);
+}
+function ztrclick() {
+  doztr(selectedztr);// alert(selectedztr);
+}
+function doztr(selectedztr) {
+	var text = inputText.value;
+	switch(selectedztr) {
+	case "unicode_to_inglish" : outputText.innerText = u_to_a(text,false); break;
+	case "inglish_to_vovelhindi" : outputText.innerText = i_to_vhin(text); break;
+	case "unicode_to_vovelhindi" : outputText.innerText = u_to_u8(text,false);break;
     }
 }
