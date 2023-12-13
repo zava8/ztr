a43b8ce00d790ae737d7fc19077ede84931919f6
zava810 <vimalkg@gmail.com>
2023-12-13 15:32:25 +0530

    input output tekst made local

--- peztransliterate/transliterate.js
+++ peztransliterate/transliterate.js
@@ -1,12 +1,8 @@
 // const u_to_a = require('./u_to_a.js');
 var inputText = document.querySelector('#ita');
 var outputText = document.querySelector('#output-text');
-// var buttonTranslate = document.querySelector('#btn-translate');
-// var apiUrl = "https://api.funtranslations.com/translate/minion.json";
 var selectedztr = null;
 function errorHandle(error) { 
-	// In case error occurs the errorHandle
-	// function will be called
 	alert('Error occurred')
 	console.log("error occurred", error);
 }
@@ -20,20 +16,17 @@ function ztrclick() {
 function i_to_u8hin_click(){ doztr("i_to_u8hin"); }
 function i_to_u8pnz_click(){ doztr("i_to_u8pnz"); }
 function i_to_u8tmil_click(){ doztr("i_to_u8tmil"); }
-function u_to_u8hin_click(){
-	doztr("u_to_u8hin");
-}
-function u_to_u8pnz_click(){
-	doztr("u_to_u8pnz");
-}
+function u_to_u8hin_click(){ doztr("u_to_u8hin"); }
+function u_to_u8pnz_click(){ doztr("u_to_u8pnz"); }
 function doztr(selectedztr) {
-	var text = inputText.value;
+	let ota = document.querySelector('#output-text');
+	const itekst = document.querySelector('#input-text').value;
 	switch(selectedztr) {
-	case "unicode_to_inglish" : outputText.innerText = u_to_a(text,false); break;
-	case "i_to_u8hin" : outputText.innerText = i_to_u8hin(text); break;
-	case "i_to_u8pnz" : outputText.innerText = i_to_u8pnz(text); break;
-	case "i_to_u8tmil" : outputText.innerText = i_to_u8tmil(text); break;
-	case "u_to_u8hin" : outputText.innerText = u_to_u8hin(text,false);break;
-	case "u_to_u8pnz" : outputText.innerText = u_to_u8pnz(text,false);break;
+	case "unicode_to_inglish" : ota.innerText = u_to_a(itekst,false); break;
+	case "i_to_u8hin" : ota.innerText = i_to_u8hin(itekst); break;
+	case "i_to_u8pnz" : ota.innerText = i_to_u8pnz(itekst); break;
+	case "i_to_u8tmil" : ota.innerText = i_to_u8tmil(itekst); break;
+	case "u_to_u8hin" : ota.innerText = u_to_u8hin(itekst,false);break;
+	case "u_to_u8pnz" : ota.innerText = u_to_u8pnz(itekst,false);break;
     }
 }
